2220b9624b5211ace0979223aaa6b59dad337ac1
fedyaslonn <133713839+fedyaslonn@users.noreply.github.com>
2024-04-29 00:47:45 +0300

    Add files via upload

--- task4/main.py
+++ task4/main.py
@@ -1,1 +1,27 @@
+def read_words(filename):
+    with open(filename, 'r', encoding='UTF-8') as file:
+        words = file.read().split()
+        words = [word.strip().lower() for word in words]
+    return words
 
+def replace_substr(line, worlds_to_replace_set):
+    for word in worlds_to_replace_set:
+        index = line.lower().find(word)
+        while index != -1:
+            line = line[:index] + '*' * len(word) + line[index+len(word):]
+            index = line.lower().find(word, index + len(word))
+    return line
+def replace_words(filename, file_to_read, file_to_write):
+    words_to_replace = read_words(filename)
+    words_to_replace_set = set(words_to_replace)
+
+    with open(file_to_read, 'r', encoding='UTF-8') as file:
+        lines = file.readlines()
+
+
+    with open(file_to_write, 'w', encoding='UTF-8') as new_file:
+        for line in lines:
+            res_line = replace_substr(line, words_to_replace_set)
+            new_file.write(res_line)
+
+tot_res = replace_words('read.txt', 'input.txt', 'output.txt')
