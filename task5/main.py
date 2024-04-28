f3a2b74f63c0a10ebd80abd30888abc3a69af2cb
fedyaslonn <133713839+fedyaslonn@users.noreply.github.com>
2024-04-29 00:49:09 +0300

    Add files via upload

--- task5/main.py
+++ task5/main.py
@@ -1,1 +1,13 @@
+def foo(input_file, output_file):
+    with open(input_file, 'r') as f:
+        lines = [line.strip() for line in f.readlines()]
+    with open (output_file, 'w') as f:
+        for line in lines:
+            new_line = line.split()
+            if new_line[2] in "012":
+                f.write(f"{new_line[0]} {new_line[1]} \n")
 
+
+
+
+foo('input.txt', 'output.txt')
